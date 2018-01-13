417ab14dbedf8e3788d6fda91270553c06cd192a
chenshuijin <785795635@qq.com>
2018-01-14 01:46:17 +0800

    fix complile fail error

--- erc20/epcsale.sol
+++ erc20/epcsale.sol
@@ -1,30 +1,30 @@
 pragma solidity ^0.4.13;
 
 contract Math {
-  function add(uint256 x, uint256 y) constant internal returns (uint256 z) {
+  function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
     assert((z = x + y) >= x);
   }
 
-  function sub(uint256 x, uint256 y) constant internal returns (uint256 z) {
+  function sub(uint256 x, uint256 y) internal pure returns (uint256 z) {
     assert((z = x - y) <= x);
   }
 
-  function mul(uint256 x, uint256 y) constant internal returns (uint256 z) {
+  function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
     assert((z = x * y) >= x);
   }
 
-  function div(uint256 x, uint256 y) constant internal returns (uint256 z) {
+  function div(uint256 x, uint256 y) internal pure returns (uint256 z) {
     z = x / y;
   }
 }
 
 contract Token {
   uint256 public totalSupply;
-  function balanceOf(address _owner) constant returns (uint256 balance);
-  function transfer(address _to, uint256 _value) returns (bool success);
-  function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-  function approve(address _spender, uint256 _value) returns (bool success);
-  function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+  function balanceOf(address _owner) public constant returns (uint256 balance);
+  function transfer(address _to, uint256 _value) public returns (bool success);
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+  function approve(address _spender, uint256 _value) public returns (bool success);
+  function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
   event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
@@ -32,11 +32,11 @@ contract Token {
 /*  ERC 20 token */
 contract ERC20 is Token {
 
-  function name() public constant returns (string) { name; }
-  function symbol() public constant returns (string) { symbol; }
-  function decimals() public constant returns (uint8) { decimals; }
+  function name() public pure returns (string) { name; }
+  function symbol() public pure returns (string) { symbol; }
+  function decimals() public pure returns (uint8) { decimals; }
 
-  function transfer(address _to, uint256 _value) returns (bool success) {
+  function transfer(address _to, uint256 _value) public returns (bool success) {
     if (balances[msg.sender] >= _value && _value > 0) {
       balances[msg.sender] -= _value;
       balances[_to] += _value;
@@ -47,7 +47,7 @@ contract ERC20 is Token {
     }
   }
 
-  function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
     if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
       balances[_to] += _value;
       balances[_from] -= _value;
@@ -59,17 +59,17 @@ contract ERC20 is Token {
     }
   }
 
-  function balanceOf(address _owner) constant returns (uint256 balance) {
+  function balanceOf(address _owner) public constant returns (uint256 balance) {
     return balances[_owner];
   }
 
-  function approve(address _spender, uint256 _value) returns (bool success) {
+  function approve(address _spender, uint256 _value) public returns (bool success) {
     allowed[msg.sender][_spender] = _value;
     Approval(msg.sender, _spender, _value);
     return true;
   }
 
-  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+  function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
     return allowed[_owner][_spender];
   }
 
@@ -80,7 +80,7 @@ contract ERC20 is Token {
 contract owned {
   address public owner;
 
-  function owned() {
+  function owned() public {
     owner = msg.sender;
   }
 
@@ -89,7 +89,7 @@ contract owned {
     _;
   }
 
-  function transferOwnership(address newOwner) onlyOwner {
+  function transferOwnership(address newOwner) public onlyOwner {
     owner = newOwner;
   }
 }
@@ -111,8 +111,7 @@ contract EPCToken is ERC20, Math, owned {
    string _name,
    string _symbol,
    string _version
-  )
-  {
+  ) public {
     name = _name;
     symbol = _symbol;
     version = _version;
@@ -121,7 +120,7 @@ contract EPCToken is ERC20, Math, owned {
   /*
    * mint token
    */
-  function mintToken(address target, uint256 mintedAmount) onlyOwner {
+  function mintToken(address target, uint256 mintedAmount) public onlyOwner {
     balances[target] += mintedAmount;
     totalSupply += mintedAmount;
     MintToken(target, mintedAmount);
@@ -130,7 +129,7 @@ contract EPCToken is ERC20, Math, owned {
   /*
    * burn the tokens, cant never get back
    */
-  function burn(uint256 amount) returns (bool success) {
+  function burn(uint256 amount) public returns (bool success) {
     require(balances[msg.sender] >= amount);
     balances[msg.sender] -= amount;
     totalSupply -= amount;
@@ -141,7 +140,7 @@ contract EPCToken is ERC20, Math, owned {
   /*
    * reward token
    */
-  function reward(address target, uint256 amount) onlyOwner {
+  function reward(address target, uint256 amount) public onlyOwner {
     balances[target] += amount;
     Reward(target, amount);
   }
@@ -150,7 +149,7 @@ contract EPCToken is ERC20, Math, owned {
    * kill the contract from the blockchain
    * and send the balance to the owner
    */
-  function kill() onlyOwner {
+  function kill() public onlyOwner {
     selfdestruct(owner);
   }
 }
@@ -171,7 +170,7 @@ contract EPCSale is Math, owned {
    uint256 _fundingStartBlock,
    uint256 _fundingEndBlock
   )
-  {
+  public {
     isFinalized = false; //controls pre through crowdsale state
     epc = EPCToken(_epc);
     fundingStartBlock = _fundingStartBlock;
@@ -181,7 +180,7 @@ contract EPCSale is Math, owned {
   /*
    * crowdsale
    */
-  function crowdSale() payable {
+  function crowdSale() public payable {
     require(!isFinalized);
     assert(block.number >= fundingStartBlock);
     assert(block.number <= fundingEndBlock);
@@ -195,7 +194,7 @@ contract EPCSale is Math, owned {
   /*
    * caculate the crowdsale rate per eth
    */
-  function exchangeRate() constant returns(uint256) {
+  function exchangeRate() public constant returns(uint256) {
     if (block.number<=fundingStartBlock+43200) return 10000; // early price
     if (block.number<=fundingStartBlock+2*43200) return 8000; // crowdsale price
     return 7000; // default price
@@ -204,7 +203,7 @@ contract EPCSale is Math, owned {
   /*
    * unit test for crowdsale exchange rate
    */
-  function testExchangeRate(uint blockNumber) constant returns(uint256) {
+  function testExchangeRate(uint blockNumber) public constant returns(uint256) {
     if (blockNumber <= fundingStartBlock+43200) return 10000; // early price
     if (blockNumber <= fundingStartBlock+2*43200) return 8000; // crowdsale price
     return 7000; // default price
@@ -213,7 +212,7 @@ contract EPCSale is Math, owned {
   /*
    * unit test for calculate funded amount
    */
-  function testFunded(uint256 amount) constant returns(uint256) {
+  function testFunded(uint256 amount) public constant returns(uint256) {
     uint256 tokens = mul(amount, exchangeRate());
     return add(funded, tokens);
   }
@@ -221,35 +220,35 @@ contract EPCSale is Math, owned {
   /*
    * unamed function for crowdsale
    */
-  function () payable {
+  function () public payable {
     crowdSale();
   }
 
   /*
    * withrawal the crowd eth
    */
-  function withdrawal() onlyOwner {
+  function withdrawal() public onlyOwner {
     msg.sender.transfer(this.balance);
   }
 
   /*
    * stop the crowdsale
    */
-  function stop() onlyOwner {
+  function stop() public onlyOwner {
     isFinalized = true;
   }
 
   /*
    * start the crowdsale
    */
-  function start() onlyOwner {
+  function start() public onlyOwner {
     isFinalized = false;
   }
 
   /*
    * retrieve tokens from the contract
    */
-  function retrieveTokens(uint256 amount) onlyOwner {
+  function retrieveTokens(uint256 amount) public onlyOwner {
     assert(epc.transfer(owner, amount));
   }
 
@@ -257,7 +256,7 @@ contract EPCSale is Math, owned {
    * kill the contract from the blockchain
    * and retrieve the tokens and balance to the owner
    */
-  function kill() onlyOwner {
+  function kill() public onlyOwner {
     epc.transfer(owner, epc.balanceOf(this));
     selfdestruct(owner);
   }
